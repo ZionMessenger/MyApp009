8eaf222bb0e1993ac9ee6a08386c745f6d2c83b4
ZionMessenger <john.vu.itm@gmail.com>
2021-10-17 22:24:35 +0700

    V02
    My Second Commit

--- app/src/main/java/com/example/myapp09/Retrofit2/DataClient.java
+++ app/src/main/java/com/example/myapp09/Retrofit2/DataClient.java
@@ -8,9 +8,11 @@ import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Query;
 
 // giao tiếp data
 public interface DataClient {
@@ -32,4 +34,9 @@ public interface DataClient {
     @POST("login.php")
     Call<List<Sinhvien>> Logindata(@Field("taikhoan") String taikhoan,
                                    @Field("matkhau") String matkhau);
+
+    //20211017: dekete
+    @GET("delete.php")
+    Call<String> DeleteData(@Query("id") String id,
+                            @Query("hinhanh") String hinhanh);
 }
--- app/src/main/java/com/example/myapp09/ThongtinUserActivity.java
+++ app/src/main/java/com/example/myapp09/ThongtinUserActivity.java
@@ -4,17 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.myapp09.Retrofit2.APIUtils;
+import com.example.myapp09.Retrofit2.DataClient;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ThongtinUserActivity extends AppCompatActivity {
     //khai bao
     Button btnDelete;
     TextView txtTaikhoan, txtMatkhau;
     ImageView imgUser;
+    ArrayList<Sinhvien> sinhvienArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +33,37 @@ public class ThongtinUserActivity extends AppCompatActivity {
         anhxa();
         //khoi tao
         init();
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String namefolder = sinhvienArrayList.get(0).getHinhanh();
+                namefolder = namefolder.substring(namefolder.lastIndexOf("/")); //thêm "/" vào chuỗi hình
+                //Log.d("BBB",namefolder);
+
+                DataClient dataClient = APIUtils.getData();
+                Call<String> callback = dataClient.DeleteData(sinhvienArrayList.get(0).getId(),namefolder);
+                callback.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        String ketqua = response.body();
+                        if (ketqua.equals("Success")){
+                            Log.d("BBB","OK");
+                            finish();
+                        }else {
+                            Log.d("BBB","Fail");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+
+                    }
+                });
+
+
+
+            }
+        });
 
 
 
@@ -31,11 +72,12 @@ public class ThongtinUserActivity extends AppCompatActivity {
     private void init() {
         //Lay thong tin ve tu man hinh Main
         Intent intent = getIntent();
-        ArrayList<Sinhvien> sinhvienArrayList = intent.getParcelableArrayListExtra("mangsinhvien");
+        sinhvienArrayList = intent.getParcelableArrayListExtra("mangsinhvien");
         //Log.d("BBB", sinhvienArrayList.get(0).getTaikhoan());
         //Log.d("BBB", sinhvienArrayList.get(0).getMatkhau());
         txtTaikhoan.setText(sinhvienArrayList.get(0).getTaikhoan());
         txtMatkhau.setText(sinhvienArrayList.get(0).getMatkhau());
+        Picasso.get().load(sinhvienArrayList.get(0).getHinhanh()).into(imgUser);
 
     }
 
